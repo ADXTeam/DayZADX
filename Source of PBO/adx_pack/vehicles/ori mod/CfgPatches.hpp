70b69ee75af5aadac6b9633eb6d603660dea0eed
KevelTV <keveltv@gmail.com>
2013-11-08 18:52:06 +0400

    delete not need pack files

--- Source of PBO/adx_pack/vehicles/ori mod/CfgPatches.hpp
+++
@@ -1,8 +0,0 @@
-class CfgPatches {
-	class ori_vil_pack {
-		units[] = {"ori_vil_originsmod_lublin_truck", "ori_vil_originsmod_truck_civ", "ori_vil_originsmod_truck_civ1", "ori_vil_originsmod_truck_civ2", "ori_vil_originsmod_volvo_fl290", "ori_p85_originsmod_CUCV", "ori_p85_originsmod_cucv_pickup", "ori_vil_lada_2105_rust", "ori_poldek", "ori_poldek_black", "ori_transit", "ori_transit_blue", "ori_survivorBus"};
-		weapons[] = {};
-		requiredVersion = 0.1;
-		requiredAddons[] = {"CAWater2", "CAWheeled", "CAWheeled_E"};
-	};
-};
